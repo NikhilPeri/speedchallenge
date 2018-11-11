ac793bc152133e724af3c1d306466c906f7daf48
NikhilPeri <nikhil.peri96@gmail.com>
2018-11-10 22:50:04 -0500

    working train model

--- algorithms/model.py
+++ algorithms/model.py
@@ -1,29 +1,28 @@
-from keras.models import Sequential
-from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
-
-BATCH_SIZE=30
-EPOCHS=100
-
 cnn_model = Sequential()
-cnn_model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(480, 640, 3)))
-cnn_model.add(Conv2D(32, (3, 3), activation='relu'))
+cnn_model.add(Conv2D(32, (5, 5), padding='same', activation='relu', input_shape=(480, 640, 3)))
+cnn_model.add(Conv2D(32, (5, 5), activation='relu'))
 cnn_model.add(MaxPooling2D(pool_size=(2, 2)))
 cnn_model.add(Dropout(0.25))
 
-cnn_model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-cnn_model.add(Conv2D(64, (3, 3), activation='relu'))
+cnn_model.add(Conv2D(32, (5, 5), padding='same', activation='relu'))
+cnn_model.add(Conv2D(32, (5, 5), activation='relu'))
 cnn_model.add(MaxPooling2D(pool_size=(2, 2)))
 cnn_model.add(Dropout(0.25))
 
-cnn_model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-cnn_model.add(Conv2D(64, (3, 3), activation='relu'))
+cnn_model.add(Conv2D(32, (5, 5), padding='same', activation='relu'))
+cnn_model.add(Conv2D(32, (5, 5), activation='relu'))
+cnn_model.add(MaxPooling2D(pool_size=(2, 2)))
+cnn_model.add(Dropout(0.25))
+
+cnn_model.add(Conv2D(32, (5, 5), padding='same', activation='relu'))
+cnn_model.add(Conv2D(32, (5, 5), activation='relu'))
 cnn_model.add(MaxPooling2D(pool_size=(2, 2)))
 cnn_model.add(Dropout(0.25))
 
 cnn_model.add(Flatten())
-cnn_model.add(Dense(512, activation='relu'))
+cnn_model.add(Dense(20, activation='relu'))
 cnn_model.add(Dropout(0.5))
-cnn_model.add(Dense(1, activation='relu'))
+cnn_model.add(Dense(1, activation='linear'))
 
 cnn_model.summary()
-cnn_model.compile(optimizer='sgd', loss='mean_squared_error', metrics=['accuracy'])
+cnn_model.compile(optimizer=Adam(lr=0.0001, beta_1=0.0, amsgrad=True), loss='mean_squared_error', metrics=['mse'])
--- algorithms/train.py
+++ algorithms/train.py
@@ -5,37 +5,35 @@ from collections import deque
 
 from algorithms.model import cnn_model, BATCH_SIZE, EPOCHS
 
-
 video = cv2.VideoCapture('data/train.mp4')
 speed = deque(np.loadtxt('data/train.txt', delimiter='\n'))
-
 def labeled_frame():
     _, frame = video.read()
     return (speed.popleft(), cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
-
 buffer_length = cnn_model.get_input_shape_at(0)[3]
 frame_buffer=deque(maxlen=buffer_length)
 # Populate Buffer
 for i in range(buffer_length):
     frame_buffer.append(labeled_frame()[1])
-
 training_frames=[]
 training_labels=[]
-cnn_model.load_weights('model/weights.hd5')
+try:
+    cnn_model.load_weights('model/weights.hd5')
+except:
+    print "could not load previous weights"
 while(video.isOpened()):
     label, frame = labeled_frame()
-    training_labels.append(label)
+    training_labels.append([label])
     frame_buffer.append(frame)
     training_frames.append(np.stack(frame_buffer, axis=2))
 
-    if len(training_frames) == BATCH_SIZE:
+    if len(training_frames) == 200:
         frames = np.array(training_frames)
         labels = np.array(training_labels)
-        cnn_model.evaluate(frames, labels)
-        history = cnn_model.fit(frames, labels, batch_size=BATCH_SIZE, epochs=EPOCHS))
+        performance = cnn_model.evaluate(frames[:int(BATCH_SIZE/2)], labels[:int(BATCH_SIZE/2)])
+        print performance
+        cnn_model.fit(frames, labels, batch_size=BATCH_SIZE, epochs=EPOCHS)
         cnn_model.save_weights('model/weights.hd5')
-
         training_frames=[]
         training_labels=[]
-
 video.release()
