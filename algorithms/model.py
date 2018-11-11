ae3980dc1e5a2cddd26e2dc20212bc23f79daafc
NikhilPeri <nikhil.peri96@gmail.com>
2018-11-10 22:53:12 -0500

    model with randomized train

--- algorithms/model.py
+++ algorithms/model.py
@@ -1,3 +1,10 @@
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
+from keras.optimizers import Adam
+
+BATCH_SIZE=30
+EPOCHS=1
+
 cnn_model = Sequential()
 cnn_model.add(Conv2D(32, (5, 5), padding='same', activation='relu', input_shape=(480, 640, 3)))
 cnn_model.add(Conv2D(32, (5, 5), activation='relu'))
